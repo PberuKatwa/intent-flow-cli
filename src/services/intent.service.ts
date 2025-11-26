dfd941a79907f35ae8990ece4054e6c342216239
pberu8531 <pberu8531@gmail.com>
2025-11-26 18:54:16 +0300

    Initial setup v2:added logger

---
+++ src/services/intent.service.ts
@@ -0,0 +1,38 @@
+import { IntentDefinition, IntentType } from "../types/intent.types";
+
+const intents:Array<IntentDefinition> = [
+
+    {
+        id:IntentType.MAKE_ORDER,
+        label:`Make Order`,
+        phrases:[
+            
+            "place an order",
+            "make an order",
+            "i want to order",
+            "i would like to order",
+            "buy flowers"
+        ]
+    },
+    {
+        id:IntentType.TRACK_ORDER,
+        label:`Track Order`,
+        phrases:[
+            "where is my order",
+            "track my order",
+            "order status",
+            "has my order arrived",
+        ]
+    },
+    {
+        id:IntentType.PAY_FOR_ORDER,
+        label:`Pay for order`,
+        phrases:[
+            "how much do i owe",
+            "what do i need to pay",
+            "payment due",
+            "balance due",
+        ]
+    }
+
+]
---
+++ src/types/intent.types.ts
@@ -0,0 +1,12 @@
+export enum IntentType{
+    UNKNOWN = 0,
+    MAKE_ORDER = 1,
+    TRACK_ORDER = 2,
+    PAY_FOR_ORDER = 3,
+}
+
+export type IntentDefinition = {
+    id:IntentType;
+    label:string;
+    phrases:Array < string >;
+}
