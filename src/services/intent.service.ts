afe46158063830e3cb51341d765ef01a4a25d706
pberu8531 <pberu8531@gmail.com>
2025-11-26 19:51:15 +0300

    Phrase Matching v4:created definition file

--- src/services/intent.service.ts
+++ src/services/intent.service.ts
@@ -1,41 +1,40 @@
 import { IntentDefinition, IntentType } from "../types/intent.types";
 import { logger } from "../utils/logger";
 
-const allIntents:Array<IntentDefinition> = [
+export function detectIntent( allIntents:Array<IntentDefinition>, message:string ){
+    try{
 
-    {
-        id:IntentType.MAKE_ORDER,
-        label:`Make Order`,
-        phrases:[
-            
-            "place an order",
-            "make an order",
-            "i want to order",
-            "i would like to order",
-            "buy flowers"
-        ]
-    },
-    {
-        id:IntentType.TRACK_ORDER,
-        label:`Track Order`,
-        phrases:[
-            "where is my order",
-            "track my order",
-            "order status",
-            "has my order arrived",
-        ]
-    },
-    {
-        id:IntentType.PAY_FOR_ORDER,
-        label:`Pay for order`,
-        phrases:[
-            "how much do i owe",
-            "what do i need to pay",
-            "payment due",
-            "balance due",
-        ]
+        const text = message.toLowerCase().trim()
+
+        for(const intent of allIntents){
+
+            for(const phrase of intent.phrases){
+
+                if( text.includes(phrase) ){
+                    return{
+                        intent:intent.id,
+                        label:intent.label,
+                        matchedPhrases:phrase
+                    }
+                }
+
+            }
+
+        }
+
+        return{
+            intent:IntentType.UNKNOWN,
+            label:'UNKNOWN',
+            phrase:'UNKNOWN'
+        }
+
+    }catch(error:any){
+
+        logger.error(`Error in detecting intent`,{
+            errorMessage:error.message,
+            errorStack:error.stack
+        })
+
     }
 
-]
-
-export default allIntents;
+}
