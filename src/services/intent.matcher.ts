1de46d72544d1561f68b382038508847f13c2a81
pberu8531 <pberu8531@gmail.com>
2025-11-30 17:42:07 +0300

    Matcher optimization v4:fix double processing

--- src/services/intent.matcher.ts
+++ src/services/intent.matcher.ts
@@ -15,9 +15,9 @@ const SCORES = {
 export function detectIntent(intents: Array<IntentDefinition>, message: string) {
   const { stemmedTokens } = tokenize(message);
 
-  const matchedStrongTokens = []
-  const matchedFuzzyTokens = []
-  const matchedWeakTokens = []
+  const matchedStrongTokens:Array<string> = []
+  const matchedFuzzyTokens:Array<string>  = []
+  const matchedWeakTokens:Array<string>  = []
   
   let bestIntent = { 
     id: "UNKNOWN", 
@@ -44,15 +44,20 @@ export function detectIntent(intents: Array<IntentDefinition>, message: string) 
 
       const phraseTokens = tokenize(phrase).stemmedTokens;
 
-      const intersectionTokens = phraseTokens.filter( function(token){
-        return stemmedTokens.includes(token)
-      })
+      let intersectionTokens = 0;
 
-      tokenList = phraseTokens.filter( function(token){
-        return !stemmedTokens.includes(token)
-      })
+      stemmedTokens.forEach(
+        function( token, index ){
 
-      const matchRatio = ( intersectionTokens.length / phraseTokens.length )
+          if(phraseTokens.includes(token)){
+            intersectionTokens ++
+            usedTokenIndices.add(index)
+          }
+
+        }
+      )
+
+      const matchRatio = ( intersectionTokens / phraseTokens.length )
 
       if( matchRatio === 1 ){
 
@@ -64,14 +69,8 @@ export function detectIntent(intents: Array<IntentDefinition>, message: string) 
         }
 
       } else if( matchRatio < 1 ){
-        score += ( SCORES.EXACT_PHRASE * matchRatio * SCORES.PARTIAL_PHRASE_MULTIPLIER ) 
 
-        stemmedTokens.forEach((t, index) => {
-          if (phraseTokens.includes(t)) {
-            usedTokenIndices.add(index);
-            console.log(`Marked token "${t}" at index ${index} as USED`);
-          }
-        });
+        score += ( SCORES.EXACT_PHRASE * matchRatio * SCORES.PARTIAL_PHRASE_MULTIPLIER ) 
 
       }
 
@@ -91,11 +90,11 @@ export function detectIntent(intents: Array<IntentDefinition>, message: string) 
 
             score += SCORES.STRONG_TOKEN
             matchedStrongTokens.push(tok)  
-                      
+
           }else{
 
             const distance = getLevenshteinDistance(tok, sTokenized);
-            if( distance.length <= 1 ) score += SCORES.FUZZY_MATCH
+            if( distance <= 1) score += SCORES.FUZZY_MATCH
 
             matchedFuzzyTokens.push(tok)
 
@@ -115,7 +114,12 @@ export function detectIntent(intents: Array<IntentDefinition>, message: string) 
         let wTokenized = tokenizeSingleWord(wToken).stemmed
 
         for(const token of tokenList){
-          if( wTokenized === token ) score += SCORES.STRONG_TOKEN
+          if( wTokenized === token ){
+
+            score += SCORES.STRONG_TOKEN
+            matchedWeakTokens.push(token)
+
+          } 
         } 
 
       }
@@ -136,7 +140,7 @@ export function detectIntent(intents: Array<IntentDefinition>, message: string) 
   }
 
   if (bestIntent.score < SCORES.MIN_THRESHOLD) {
-    return { ...bestIntent, id: IntentType.UNKNOWN, label: "UNKNOWN" };
+    return { ...bestIntent, id: IntentType.UNKNOWN, label: "UNKNOWN",matchedFuzzyTokens, matchedStrongTokens, matchedWeakTokens };
   }
 
   return bestIntent;
