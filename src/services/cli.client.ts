0791a145e145fa2bb89769d58e5881c464d860e6
pberu8531 <pberu8531@gmail.com>
2025-12-01 16:55:25 +0300

    Cli upgrade v4:added cli name and intents

--- src/services/cli.client.ts
+++ src/services/cli.client.ts
@@ -1,17 +1,23 @@
 import * as readline from 'readline';
 import chalk from "chalk";
+import { detectIntent } from "./intent.matcher";
+import { ReadOnlyIntentDefinition } from "../types/intent.types"
 
 class CLiCLient{
     private readonly rl:readline.Interface;
+    private readonly  cliName:string;
+    private readonly intents:ReadOnlyIntentDefinition[];
 
-    constructor(promptMessage:string){
+    constructor( promptMessage:string, cliName:string, intents:ReadOnlyIntentDefinition[] ){
 
         this.rl = readline.createInterface({
             input:process.stdin,
             output:process.stdout,
-            prompt:promptMessage
+            prompt:chalk.blue(`${promptMessage} ==>`)
         });
 
+        this.cliName = cliName;
+        this.intents = intents;
         // this.rl.on('line', this.handleInput.bind(this) )
     }
 
